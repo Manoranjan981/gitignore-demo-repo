123969eab49dacc1a644243c4ed5b6e857274d67
Manoranjan981 <mano.ranjan981@gmail.com>
2022-12-15 11:55:38 +0530

    second committed with gitignore

---
+++ src/main/java/com/mano/App.java
@@ -0,0 +1,13 @@
+package com.mano;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
